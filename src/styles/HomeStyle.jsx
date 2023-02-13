2583883e6e912dc05917b9add753abe64441f269
dilshad360 <dilshad360.dev@gmail.com>
2023-02-13 11:35:05 +0530

    feat: banner

--- src/styles/HomeStyle.jsx
+++ src/styles/HomeStyle.jsx
@@ -3,10 +3,17 @@ import styled from "styled-components"
 
 export const ListHeader = styled.h1`
     margin-top: 2rem;
+    font-size: 1.6rem;
 `
 
 export const HomeWrapper = styled.div`
-    
+    padding: 0 18rem 0 18rem;
+    @media (max-width: 1024px) {
+        padding: 0;
+    }
+    @media (max-width: 768px) {
+        padding: 0;
+    }
 `
 
 
