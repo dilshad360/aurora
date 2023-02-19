b785edda4fbd22a437ca86ee87899f0c837f8474
dilshad360 <dilshad360.dev@gmail.com>
2023-02-19 13:05:58 +0530

    fix: result medal

--- src/components/ResultCard.jsx
+++ src/components/ResultCard.jsx
@@ -7,9 +7,9 @@ import ThirdMedal from '../assets/medals/third.png'
 function ResultCard({result, index}) {
   return (
     <ResultCardWrapper>  
-        { index === 0 && <img src={FirstMedal} alt="firstmedal" />}
-        { index === 1 && <img src={SecondMedal} alt="secondmedal" />}
-        { index === 2 && <img src={ThirdMedal} alt="thirdmedal" />}
+        { result.fields.Place === "First" && <img src={FirstMedal} alt="firstmedal" />}
+        { result.fields.Place === "Second" && <img src={SecondMedal} alt="secondmedal" />}
+        { result.fields.Place === "Thrid" && <img src={ThirdMedal} alt="thirdmedal" />}
         <h2>{result.fields.Name}</h2>   
         <p>{result.fields.Department}</p>
         <p>{result.fields.Year} yr</p>
